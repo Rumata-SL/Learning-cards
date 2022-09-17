8d8d2b0c1f941a9884df35c10a2bac57799df146
Mihail Schutow <schutow.mihail@yandex.ru>
2022-09-17 18:08:40 +0300

    feat superButton

--- src/components/testComponent/superComponents/superButton/SuperButton.tsx
+++ src/components/testComponent/superComponents/superButton/SuperButton.tsx
@@ -14,7 +14,7 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         ...restProps// все остальные пропсы попадут в объект restProps, там же будет children
     }
 ) => {
-    const finalClassName = `${red ? s.red : s.button} ${className}`
+    const finalClassName = `${red ?? s.red} + ${s.button}`
 
     return (
         <button
