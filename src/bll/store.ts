e7386f982f99a67e570b264fd137fc0b63d31055
Rumata-SL <kirr7619@gmail.com>
2022-09-19 00:42:05 +0500

    changed store

--- src/bll/store.ts
+++ src/bll/store.ts
@@ -1,61 +1,60 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from 'redux'
-import {
-	ProfileActionType,
-	profileReducer,
-} from '../feautures/profile/profileReducer'
 import { LoginActionType, loginReducer } from '../feautures/login/loginReducer'
 import {
-	newPasswordActionType,
-	newPasswordReducer,
+    ProfileActionType,
+    profileReducer,
+} from '../feautures/profile/profileReducer'
+import {
+    newPasswordActionType,
+    newPasswordReducer,
 } from '../feautures/newPassword/newPasswordReducer'
 import {
-	RegisteredActionType,
-	registrationReducer,
+    RegisteredActionType,
+    registrationReducer,
 } from '../feautures/registration/registrationReducer'
 import {
-	RecoveryActionType,
-	recoveryReducer,
+    RecoveryActionType,
+    recoveryReducer,
 } from '../feautures/recovery/recoveryReducer'
-
-import thunkMiddleware, { ThunkAction, ThunkDispatch } from 'redux-thunk'
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import { appReducer } from '../app/appReducer'
+import thunkMiddleware, {ThunkAction, ThunkDispatch} from "redux-thunk";
+
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 
 const rootReducer = combineReducers({
-	app: appReducer,
-	profile: profileReducer,
-	login: loginReducer,
-	newPassword: newPasswordReducer,
-	registration: registrationReducer,
-	recovery: recoveryReducer,
+    app: appReducer,
+    profile: profileReducer,
+    login: loginReducer,
+    newPassword: newPasswordReducer,
+    registration: registrationReducer,
+    recovery: recoveryReducer,
 })
 
 export const store = legacy_createStore(
-	rootReducer,
-	applyMiddleware(thunkMiddleware)
+    rootReducer,
+    applyMiddleware(thunkMiddleware)
 )
 
 export type AppStateType = ReturnType<typeof rootReducer>
 
 export type AppRootActionsType =
-	| LoginActionType
-	| RegisteredActionType
-	| RecoveryActionType
-	| newPasswordActionType
-	| ProfileActionType // your actionType
+    | LoginActionType
+    | RegisteredActionType
+    | RecoveryActionType
+    | newPasswordActionType
+    | ProfileActionType // your actionType
 
 export type ThunkType<ReturnType = void> = ThunkAction<
-	ReturnType,
-	AppStateType,
-	unknown,
-	AppRootActionsType
->
+    ReturnType,
+    AppStateType,
+    unknown,
+    AppRootActionsType
+    >
 
 export type DispatchActionType = ThunkDispatch<
-	AppStateType,
-	unknown,
-	AppRootActionsType
->
-
+    AppStateType,
+    unknown,
+    AppRootActionsType
+    >
 export const useAppDispatch = () => useDispatch<DispatchActionType>()
 export const useAppSelector: TypedUseSelectorHook<AppStateType> = useSelector
