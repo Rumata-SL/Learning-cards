36c8d738b7e20e4c6f8068d99566fd8cd036efea
Ruslan Pershin <ruslan.pershin98@gmail.com>
2022-09-18 18:55:32 +0300

    add(profileAPI, profileReduser): getMe, editMe

---
+++ src/feautures/profile/profileAPI.ts
@@ -0,0 +1,37 @@
+import {instance} from "../../api/instance";
+
+export const profileAPI = {
+    getMe() {
+        return instance.post<ProfileType>('/auth/me')
+    },
+    editMe(args: EditMeArgsType) {
+        return instance.put<EditMeArgsType, EditMeResponseType>('/auth/me', args)
+    }
+}
+
+//type
+export type ProfileType = {
+    _id: string
+    email: string
+    rememberMe: boolean
+    isAdmin: boolean
+    name: string
+    verified: boolean
+    publicCardPacksCount: number
+    created: string
+    updated: string
+    __v: number
+    token: string
+    tokenDeathTime: number
+    avatar: string
+}
+
+export type EditMeArgsType = {
+    name?: string
+    avatar?: string
+}
+
+export type EditMeResponseType = {
+    updatedUser: ProfileType
+    error?: string
+}
