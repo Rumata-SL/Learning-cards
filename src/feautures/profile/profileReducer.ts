9c6344e62241a716aa8f4443e1e39e2d27e688a5
Ruslan Pershin <ruslan.pershin98@gmail.com>
2022-09-18 18:58:44 +0300

    add(profileAPI, profileReducer): getMe, editMe

--- src/feautures/profile/profileReducer.ts
+++ src/feautures/profile/profileReducer.ts
@@ -1,20 +1,46 @@
-type InitialStateType= {
-    profile:Array<string>
+import { EditMeArgsType, profileAPI, ProfileType } from '../../api/profileAPI'
+import { AppRootActionsType, ThunkType } from '../../bll/store'
+
+//reducer
+const initialState: InitialStateType = {
+	profile: null,
+}
+export const profileReducer = (
+	state = initialState,
+	action: AppRootActionsType
+) => {
+	switch (action.type) {
+		case 'profile/SET_PROFILE':
+			return { ...state, profile: action.profile }
+		default:
+			return state
+	}
 }
 
-type ActionType = ReturnType<typeof profileAC>
-
-const initialState = ""
-
-export const profileReducer = (state=true, action:ActionType)=>{
-    switch (action.type){
-        default:
-            return state
-    }
+//action
+const setProfileAC = (profile: ProfileType) => {
+	return {
+		type: 'profile/SET_PROFILE',
+		profile,
+	} as const
 }
 
-const profileAC = ()=>{
-    return{
-        type:"PROFILE"
-    }as const
+//thunk
+export const getProfileTC = (): ThunkType => async dispatch => {
+	const res = await profileAPI.getMe()
+	dispatch(setProfileAC(res.data))
 }
+
+export const updateProfileTC =
+	(args: EditMeArgsType): ThunkType =>
+	async dispatch => {
+		const res = await profileAPI.editMe(args)
+		dispatch(getProfileTC())
+	}
+
+//type
+type InitialStateType = {
+	profile: null | ProfileType
+}
+
+export type ProfileActionType = ReturnType<typeof setProfileAC>
