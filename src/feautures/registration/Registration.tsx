75301b0f58b78e3d8e891fbcd14ee02ac7c0d8ab
Rumata-SL <kirr7619@gmail.com>
2022-09-19 14:44:03 +0500

    fix: changed styles in Registration.module.css

--- src/feautures/registration/Registration.tsx
+++ src/feautures/registration/Registration.tsx
@@ -23,7 +23,7 @@ type FormikErrorType = {
     confirmPassword?: string
 }
 
-export const Registration:FC = () => {
+export const Registration: FC = () => {
 
     const dispatch = useAppDispatch()
 
@@ -47,25 +47,25 @@ export const Registration:FC = () => {
         validate: (values) => {
             const errors: FormikErrorType = {};
             if (!values.email) {
-                errors.email = 'Required';
+                errors.email = "Enter email";
             } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-                errors.email = 'Invalid email address';
+                errors.email = "Invalid email address";
             }
             if (!values.password) {
-                errors.password = 'Required';
+                errors.password = "Enter password";
             } else if (values.password.length <= 7) {
-                errors.password = 'Password must be more than 7 characters...'
+                errors.password = "Password must be more than 7 characters..."
             }
             if (!values.confirmPassword) {
-                errors.confirmPassword = 'Required';
+                errors.confirmPassword = "Repeat  password";
             } else if (values.confirmPassword !== values.password) {
-                errors.confirmPassword = 'The password and confirmation password do not match'
+                errors.confirmPassword = "The password and confirmation password do not match"
             }
             return errors;
 
         },
         onSubmit: (values) => {
-                dispatch(registerTC(values))
+            dispatch(registerTC(values))
         },
     })
 
@@ -89,11 +89,12 @@ export const Registration:FC = () => {
     }
 
     if (isRegistered) {
-        return <Navigate to={'/login'}/>
+        return <Navigate to={"/login"}/>
     }
     return (
         <div className={style.wrapper}>
-            <form className={style.formContainer} onSubmit={formik.handleSubmit}>
+            <form className={style.formContainer}
+                  onSubmit={formik.handleSubmit}>
                 <div className={style.title}>Sign UP</div>
                 <FormControl variant="standard">
                     <InputLabel color="primary">Email</InputLabel>
@@ -105,12 +106,10 @@ export const Registration:FC = () => {
                         color={"primary"}
                         {...formik.getFieldProps("email")}
                     />
-                    {/*{(formik.errors.email && formik.touched.email) ? <div style={{color: "red"}}>{formik.errors.email}</div>:<div style={{opacity:0}}>sdrfbsdfbd</div>}*/}
                 </FormControl>
-                {/*{formik.errors.email && formik.touched.email &&
-                    <div style={{position:"fixed",top:265,left:670,color:"red" }}>
-                        <span style={{textAlign:"center"}}>{formik.errors.email}</span></div>}*/}
-
+                {formik.errors.email && formik.touched.email &&
+                    <div
+                        className={style.emailError}>{formik.errors.email}</div>}
                 <FormControl variant="standard">
                     <InputLabel color={"primary"}>Password</InputLabel>
                     <Input
@@ -136,7 +135,8 @@ export const Registration:FC = () => {
                     />
                 </FormControl>
                 {formik.errors.password && formik.touched.password &&
-                    <div style={{color:"red"}}>{formik.errors.password}</div>}
+                    <div
+                        className={style.passwordError}>{formik.errors.password}</div>}
 
                 <FormControl variant="standard">
                     <InputLabel color={"primary"}>Confirm
@@ -164,14 +164,22 @@ export const Registration:FC = () => {
                     />
                 </FormControl>
                 {formik.errors.confirmPassword && formik.touched.confirmPassword &&
-                    <div style={{position:"fixed",top:455,color:"red"}}>{formik.errors.confirmPassword}</div>}
+                    <div
+                        className={style.confirmPasswordError}>{formik.errors.confirmPassword}</div>}
                 {/*<Button color={"primary"} variant={"contained"} type="submit">Sign Up</Button>*/}
                 <SuperButton
-                    style={{fontFamily: 'Montserrat', fontStyle: "normal", fontWeight: 500, fontSize:"16px", lineHeight:"20px", letterSpacing:"0.01em"}}
+                    style={{
+                        fontFamily: "Montserrat",
+                        fontStyle: "normal",
+                        fontWeight: 500,
+                        fontSize: "16px",
+                        lineHeight: "20px",
+                        letterSpacing: "0.01em"
+                    }}
                     type="submit">Sign Up</SuperButton>
                 <span className={style.text}>Already have an account?</span>
 
-                <Link className={style.link} to={"/login"} >Sign In</Link>
+                <Link className={style.link} to={"/login"}>Sign In</Link>
 
             </form>
         </div>
