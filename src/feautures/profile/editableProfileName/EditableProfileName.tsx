bbae65bce18be6a5e10477c50f37848f062f42a5
Ruslan Pershin <ruslan.pershin98@gmail.com>
2022-09-24 20:33:05 +0300

    fix: bug EditableProfileName error

--- src/feautures/profile/editableProfileName/EditableProfileName.tsx
+++ src/feautures/profile/editableProfileName/EditableProfileName.tsx
@@ -1,5 +1,5 @@
 import {EditMeArgsType} from "../../../api/profileAPI";
-import React, {ChangeEvent, useState} from "react";
+import React, {ChangeEvent, KeyboardEvent, useState} from "react";
 import s from "./EditableProfileName.module.css";
 import {FormControl, Input, InputLabel} from "@mui/material";
 import editIcon from "../../../assets/image/icons/edit.svg";
@@ -13,36 +13,56 @@ type EditableProfileNamePropsType = {
 export const EditableProfileName: React.FC<EditableProfileNamePropsType> = ({profile, updateProfile}) => {
     const [editMode, setEditMode] = useState(false)
     const [name, setName] = useState(profile.name)
+    const [error, setError] = useState<string | null>(null)
 
     const activateEditMode = () => {
         setEditMode(true)
     }
     const activateViewMode = () => {
-        updateProfile({name})
-        setEditMode(false)
+        if (name.trim() !== '') {
+            updateProfile({name})
+            setEditMode(false)
+        } else {
+            setError('Name is required')
+        }
+
     }
     const changeName = (e: ChangeEvent<HTMLInputElement>) => {
+        setError(null)
         setName(e.currentTarget.value)
+    }
+
+    const onKeyDownHandler = (e: KeyboardEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        if (e.key === 'Enter') {
+            activateViewMode()
+        }
     }
 
     return (
         <>
             {editMode ? (
-                <div className={s.editNameBlock}>
-                    <FormControl variant='standard' className={s.editNameInput}>
-                        <InputLabel color='primary'>Nickname</InputLabel>
-                        <Input
-                            id='name'
-                            placeholder={'Nickname'}
-                            value={name}
-                            onChange={changeName}
-                            color={'primary'}
-                        />
-                    </FormControl>
-                    <button className={s.saveButton} onClick={activateViewMode}>
-                        SAVE
-                    </button>
-                </div>
+                <>
+                    <div className={s.editNameBlock}>
+                        <FormControl variant='standard' className={s.editNameInput}>
+                            <InputLabel color='primary'>Nickname</InputLabel>
+                            <Input
+                                onKeyDown={onKeyDownHandler}
+                                id='name'
+                                placeholder={'Nickname'}
+                                value={name}
+                                onChange={changeName}
+                                color={'primary'}
+                            />
+                        </FormControl>
+                        <button
+                            className={s.saveButton}
+                            onClick={activateViewMode}
+                            disabled={!!error}>
+                            SAVE
+                        </button>
+                    </div>
+                    {error && <div className={s.error}>{error}</div>}
+                </>
             ) : (
                 <div className={s.nameBlock}>
                     <h4 className={s.name}>{profile.name}</h4>
