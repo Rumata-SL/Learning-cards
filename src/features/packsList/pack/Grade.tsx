77e54b574935a70d0627c27e5c2d9e82f6c3b507
Денис <denisegorenko1@gmail.com>
2022-09-27 16:35:05 +0300

    Pack page: layout draft v3 w/ eslint

--- src/features/packsList/pack/Grade.tsx
+++ src/features/packsList/pack/Grade.tsx
@@ -1,21 +1,20 @@
-/* eslint-disable */
+import React from 'react'
 
-import React from 'react';
-import StarOutlinedIcon from '@mui/icons-material/StarOutlined';
+import StarOutlinedIcon from '@mui/icons-material/StarOutlined'
 
 type gradePropsType = {
-    grade: number
+  grade: number
 }
 export const Grade: React.FC<gradePropsType> = props => {
+  const roundedGrade = Number(props.grade.toFixed())
 
-    const roundedGrade = Number((props.grade).toFixed())
-
-    return <div>
-        <StarOutlinedIcon sx={roundedGrade >= 1 ? {color: '#FFC700'} : {color: '#DADADA'}}/>
-        <StarOutlinedIcon sx={roundedGrade >= 2 ? {color: '#FFC700'} : {color: '#DADADA'}}/>
-        <StarOutlinedIcon sx={roundedGrade >= 3 ? {color: '#FFC700'} : {color: '#DADADA'}}/>
-        <StarOutlinedIcon sx={roundedGrade >= 4 ? {color: '#FFC700'} : {color: '#DADADA'}}/>
-        <StarOutlinedIcon sx={roundedGrade >= 5 ? {color: '#FFC700'} : {color: '#DADADA'}}/>
+  return (
+    <div>
+      <StarOutlinedIcon sx={roundedGrade >= 1 ? { color: '#FFC700' } : { color: '#DADADA' }} />
+      <StarOutlinedIcon sx={roundedGrade >= 2 ? { color: '#FFC700' } : { color: '#DADADA' }} />
+      <StarOutlinedIcon sx={roundedGrade >= 3 ? { color: '#FFC700' } : { color: '#DADADA' }} />
+      <StarOutlinedIcon sx={roundedGrade >= 4 ? { color: '#FFC700' } : { color: '#DADADA' }} />
+      <StarOutlinedIcon sx={roundedGrade >= 5 ? { color: '#FFC700' } : { color: '#DADADA' }} />
     </div>
-
+  )
 }
