eccde6c5fc52097ab12fbae6e375060cd827a465
Mihail Schutow <schutow.mihail@yandex.ru>
2022-09-30 14:38:10 +0300

    feat pack reducer

--- src/features/packsList/pack/packReducer.ts
+++ src/features/packsList/pack/packReducer.ts
@@ -1,51 +1,32 @@
 /* eslint-disable */
 import {ThunkType} from '../../../bll/store'
 import {setAppStatusAC} from '../../../app/appReducer';
-import {packAPI, RequestCardsType} from '../../../api/cards/packAPI';
+import {CreateCardType, packAPI, RequestCardsType, UpdateCardType} from '../../../api/cards/packAPI';
+import {AxiosError} from 'axios';
+import {utilsError} from '../../../utils/utils_error';
 
 //initial state
-const initialState: PackType = {
-  cards: [
-    {
-      answer: 'no answer',
-      question: 'no question',
-      cardsPack_id: '5eb6a2f72f8g49402d46c6ac43',
-      grade: 2.987525071790364,
-      shots: 1,
-      user_id: '142151531535151',
-      created: '2020-05-13T11:05:44.867Z',
-      updated: '2020-03-13T11:05:44.867Z',
-      _id: '5ebbdfbf48876810f1adk0e7ece3',
-    },
-    {
-      answer: 'no answer',
-      question: 'no question',
-      cardsPack_id: '5eb6a2f72f8g49402d46c6ac43',
-      grade: 3.987525071790364,
-      shots: 1,
-      user_id: '142151531535151',
-      created: '2020-05-13T11:05:44.867Z',
-      updated: '2020-09-13T11:05:44.867Z',
-      _id: '5ebbdf4887681f0f1adk0evf7ece3',
-    },
-    {
-      answer: 'no answer',
-      question: 'no question',
-      cardsPack_id: '5eb6a2f72f8g49402d46c6ac43',
-      grade: 4.457525071790364,
-      shots: 1,
-      user_id: '142151531535151',
-      created: '2020-10-13T11:05:44.867Z',
-      updated: '2020-10-13T11:05:44.867Z',
-      _id: '5ebbgdf4887681g0f1adk0e7ece3',
-    },
-  ],
-  cardsTotalCount: 3,
-  maxGrade: 4.987525071790364,
-  minGrade: 2.0100984354076568,
-  page: 1,
-  pageCount: 4,
+const initialState = {
+  cards: [] as Array<CardType>,
+  searchData: {
+    cardAnswer: 'english',
+    cardQuestion: 'english',
+    cardsPack_id: '',
+    min: 0,
+    max: 100,
+    sortCards: '',
+    page: 1,
+    pageCount: 10
+  },
+  packCreated: '2022-09-30T08:07:31.809Z',
+  packUpdated: '2022-09-30T08:48:40.312Z',
   packUserId: '6328b5b0da5de300045fa02a',
+  packName: 'initial',
+  packPrivate: false,
+  packDeckCover: null,
+  cardsTotalCount: 0,
+  token: 'b9e18f40-4097-11ed-a346-336d45d0120e',
+  tokenDeathTime: 1664536392116,
   // packUserId: '5eecf82a3ed8f700042f1186',
 }
 
@@ -54,7 +35,17 @@ const initialState: PackType = {
 export const packReducer = (state: PackType = initialState, action: PackActionType): PackType => {
   switch (action.type) {
     case 'pack/SET-PACK':
-      return action.pack
+      return {...state, cards: action.cards}
+    case 'pack/SET-MIN-GRADE':
+      return {...state, searchData: {...state.searchData, min: action.min}}
+    case 'pack/SET-MAX-GRADE':
+      return {...state, searchData: {...state.searchData, max: action.max}}
+    case 'pack/SET-USER-ID':
+      return {...state, packUserId: action.packUserId}
+    case 'pack/SET-PAGE':
+      return {...state, searchData: {...state.searchData, page: action.page}}
+    case 'pack/SET-PAGE-COUNT':
+      return {...state, searchData: {...state.searchData, pageCount: action.pageCount}}
     /*case 'pack/DELETE-CARD':
       return {...state, cards: state.cards.filter(el => el._id !== action.cardID)}*/
     default:
@@ -63,50 +54,88 @@ export const packReducer = (state: PackType = initialState, action: PackActionTy
 }
 
 //AC
-const getPackAC = (pack: PackType) =>
-  ({
-    type: 'pack/SET-PACK',
-    pack
-  } as const)
+const setPackAC = (cards: Array<CardType>) => ({type: 'pack/SET-PACK', cards} as const)
 
-/*const addCardAC = () => {
-  return {
-    type: 'pack/ADD-CARD',
-  } as const
-}
+const setUserIDAC = (packUserId: string) => ({type: 'pack/SET-USER-ID', packUserId} as const)
 
-const updateCardAC = () => {
+const setCardsTotalCountAC = (cardsTotalCount: number) => ({
+  type: 'pack/SET-CARDS-TOTAL-COUNT',
+  cardsTotalCount
+} as const)
+const setPageAC = (page: number) => ({type: 'pack/SET-PAGE', page} as const)
+const setPageCountAC = (pageCount: number) => ({type: 'pack/SET-PAGE-COUNT', pageCount} as const)
 
-}
+const setMinGradeAC = (min: number) => ({type: 'pack/SET-MIN-GRADE', min} as const)
 
-const deleteCardAC = (cardID: string) => {
-  return {
-    type: 'pack/DELETE-CARD',
-    cardID
-  } as const
-}*/
+const setMaxGradeAC = (max: number) => ({type: 'pack/SET-MAX-GRADE', max} as const)
+
 
 //TC
-export const getPackTC = (cardsPack_id: string, params: RequestCardsType): ThunkType => async dispatch => {
+export const getPackTC = (cardsPack_id: string): ThunkType => async (dispatch, getState) => {
+  const params = getState().pack.searchData
   dispatch(setAppStatusAC('loading'))
   try {
     const res = await packAPI.getCards(cardsPack_id, params)
+    dispatch(setPackAC(res.data.cards))
+    dispatch(setUserIDAC(res.data.packUserId))
+    dispatch(setCardsTotalCountAC(res.data.cardsTotalCount))
+    dispatch(setPageAC(res.data.page))
+    dispatch(setPageCountAC(res.data.pageCount))
   } catch (e) {
+    const err = e as Error | AxiosError<{ error: string }>
+    utilsError(err, dispatch)
+  } finally {
+    dispatch(setAppStatusAC('idle'))
+  }
+}
+
+export const createCardTC = (data: CreateCardType): ThunkType => async dispatch => {
+  dispatch(setAppStatusAC('loading'))
+  try {
+    const res = await packAPI.createCard(data)
+    if (res) {
+      dispatch(getPackTC(data.cardsPack_id))
+    }
+  } catch (e) {
+    const err = e as Error | AxiosError<{ error: string }>
+    utilsError(err, dispatch)
+  } finally {
+    dispatch(setAppStatusAC('idle'))
+  }
+}
+
+export const updateCardTC = (cardsPack_id: string, data: UpdateCardType): ThunkType => async dispatch => {
+  dispatch(setAppStatusAC('loading'))
+  try {
+    const res = await packAPI.updateCard(data)
+    if (res) {
+      dispatch(getPackTC(cardsPack_id))
+    }
+  } catch (e) {
+    const err = e as Error | AxiosError<{ error: string }>
+    utilsError(err, dispatch)
+  } finally {
+    dispatch(setAppStatusAC('idle'))
+  }
+}
+
+export const deleteCardTC = (cardsPack_id: string, _id: string): ThunkType => async dispatch => {
+  dispatch(setAppStatusAC('loading'))
+  try {
+    const res = await packAPI.deleteCard(_id)
+    if (res) {
+      dispatch(getPackTC(cardsPack_id))
+    }
+  } catch (e) {
+    const err = e as Error | AxiosError<{ error: string }>
+    utilsError(err, dispatch)
   } finally {
     dispatch(setAppStatusAC('idle'))
   }
 }
 
 //types
-type PackType = {
-  cards: Array<CardType>
-  cardsTotalCount: number
-  maxGrade: number
-  minGrade: number
-  page: number
-  pageCount: number
-  packUserId: string
-}
+type PackType = typeof initialState
 
 type CardType = {
   answer: string
@@ -120,8 +149,21 @@ type CardType = {
   _id: string
 }
 
-export type PackActionType = GetPackACType /*| AddCardACType | DeleteCardACType*/
+export type PackActionType =
+  GetPackACType
+  | SetMinGradeACType
+  | SetMaxGradeACType
+  | SetUSerIDACType
+  | SetCardsTotalCountACType
+  | SetPageACType
+  | SetPageCountACType/*| AddCardACType | DeleteCardACType*/
 
-type GetPackACType = ReturnType<typeof getPackAC>
+type GetPackACType = ReturnType<typeof setPackAC>
+type SetMinGradeACType = ReturnType<typeof setMinGradeAC>
+type SetMaxGradeACType = ReturnType<typeof setMaxGradeAC>
+type SetUSerIDACType = ReturnType<typeof setUserIDAC>
+type SetCardsTotalCountACType = ReturnType<typeof setCardsTotalCountAC>
+type SetPageACType = ReturnType<typeof setPageAC>
+type SetPageCountACType = ReturnType<typeof setPageCountAC>
 /*type AddCardACType = ReturnType<typeof addCardAC>
 type DeleteCardACType = ReturnType<typeof deleteCardAC>*/
