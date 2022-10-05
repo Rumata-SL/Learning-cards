409110e65691363d90bab78c6556b11c809485db
Rumata-SL <kirr7619@gmail.com>
2022-10-05 15:39:15 +0500

    style: changed style in Pack.module.css, StyleFofModal.ts

--- src/features/modal/StyleFofModal.ts
+++ src/features/modal/StyleFofModal.ts
@@ -1,6 +1,6 @@
 export const style = {
   position: 'absolute' as 'absolute',
-  top: '50%',
+  top: '40%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
   width: 400,
@@ -8,9 +8,6 @@ export const style = {
   border: '2px solid #000',
   boxShadow: 24,
   p: 4,
-  /*display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'space-between',*/
 }
 
 export const styleBtn = {
@@ -22,8 +19,3 @@ export const styleBtn = {
 export const styleSelect = {
   marginTop: '25px',
 }
-/*export const styleBtnVariant = {
-  width: '127px',
-  height: '30px',
-  borderRadius: '30px',
-}*/
