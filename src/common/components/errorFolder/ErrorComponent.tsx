0583a733eb07304f277eaec20b4d052861a2c9a6
Ruslan Pershin <ruslan.pershin98@gmail.com>
2022-10-08 20:42:27 +0300

    feat: error component

--- src/common/components/errorFolder/ErrorComponent.tsx
+++ src/common/components/errorFolder/ErrorComponent.tsx
@@ -1,15 +1,25 @@
 import React from 'react'
 
-import e from './ErrorComponent.module.css'
+import { NavLink } from 'react-router-dom'
+
+import errorImg from '../../../assets/image/404.svg'
+import { PATH } from '../../enum/path'
+import SuperButton from '../superButton/SuperButton'
+
+import s from './ErrorComponent.module.css'
 
 const ErrorComponent = () => {
   return (
-    <div className={e.wrapper}>
-      <div className={e.title}>
-        <span>404</span>
+    <div className={s.wrapper}>
+      <div className={s.text}>
+        <h2 className={s.title}>Ooops!</h2>
+        <span className={s.subTitle}>Sorry! Page not found!</span>
+        <NavLink to={PATH.PACKS_LIST}>
+          <SuperButton className={s.button}>Back to home page</SuperButton>
+        </NavLink>
       </div>
-      <div className={e.text}>
-        <span>NOT FOUND</span>
+      <div>
+        <img src={errorImg} alt="error" />
       </div>
     </div>
   )
